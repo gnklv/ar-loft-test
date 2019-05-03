6eb7177ca055f71dc857a8e554b95dcc5d480ca1
Georgy Nikolaev <wgg357@gmail.com>
2019-05-03 19:11:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = express();
 
-var port = process.env.PORT || 5000;
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.listen(port);
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
 
-console.log('server started '+ port);
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'))
+});
+
+const server = app.listen(process.env.PORT || 8080, function () {
+  const port = server.address().port;
+  console.log('App now running on port', port)
+});
